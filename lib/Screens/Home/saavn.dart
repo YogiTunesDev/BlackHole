34af55bb4b451add55350dba94e99b8070d8f857
Rajan Meghani <rkmeghani0@gmail.com>
2021-12-15 18:01:48 +0530

    display popup in remove song

--- lib/Screens/Home/saavn.dart
+++ lib/Screens/Home/saavn.dart
@@ -1,5 +1,6 @@
 import 'package:blackhole/APIs/api.dart';
 import 'package:blackhole/CustomWidgets/gradient_containers.dart';
+import 'package:blackhole/Screens/Common/popup_loader.dart';
 import 'package:blackhole/Screens/Common/song_list.dart';
 import 'package:blackhole/Screens/Player/audioplayer.dart';
 import 'package:blackhole/model/home_model.dart';
@@ -742,58 +743,7 @@ class HeaderTitle extends StatelessWidget {
   }
 }
 
-popupLoader(BuildContext context, String message) {
-  Dialog mainDialog = Dialog(
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(12),
-    ),
-    elevation: 0.0,
-    backgroundColor: Colors.transparent,
-    child: Center(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.width / 2,
-        width: MediaQuery.of(context).size.width / 2,
-        child: Card(
-          elevation: 10,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          clipBehavior: Clip.antiAlias,
-          child: GradientContainer(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.width / 7,
-                    width: MediaQuery.of(context).size.width / 7,
-                    child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        Theme.of(context).colorScheme.secondary,
-                      ),
-                      strokeWidth: 5,
-                    ),
-                  ),
-                  Text(
-                    message,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-  return showDialog(
-    barrierDismissible: false,
-    context: context,
-    builder: (BuildContext context) => WillPopScope(
-      onWillPop: () async => false,
-      child: mainDialog,
-    ),
-  );
-}
+
 
 class SongItem extends StatelessWidget {
   final String itemImage;
