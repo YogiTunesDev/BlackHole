dfd84b1c6aff9d04b640e99ff4f85e57882fb05e
Scott Vrooman <scott.m.vrooman@gmail.com>
2023-08-11 12:33:14 -0700

    replace streming witrh streaming

--- lib/Screens/Settings/setting.dart
+++ lib/Screens/Settings/setting.dart
@@ -1390,7 +1390,7 @@ class _SettingPageState extends State<SettingPage> {
                           bottom: 10,
                         ),
                         child: Text(
-                          'High quality streming is 320kbps, normal streaming quality is 96kb.',
+                          'High quality streaming is 320kbps, normal streaming quality is 96kb.',
                           style: TextStyle(
                             fontSize: 13,
                             color: Theme.of(context).colorScheme.primary,
